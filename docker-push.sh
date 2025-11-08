b314272e830c3da104fc8e11a1f44329b705fb4a
Javaria <128834541+JavariaTabassum@users.noreply.github.com>
2025-11-08 23:03:06 +0500

    Update

--- docker-push.sh
+++ docker-push.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Login to Docker Hub
 docker login
 
-# Build the images
 docker compose build
 
-# Push the images to Docker Hub
-docker push ${DOCKER_USERNAME}/todo-mern-frontend:latest
-docker push ${DOCKER_USERNAME}/todo-mern-backend:latest
+docker push javaria725/todo-mern-frontend:latest
+docker push javaria725/todo-mern-backend:latest
